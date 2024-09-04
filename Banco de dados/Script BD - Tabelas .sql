2a3e5dca01da125ad80f2102b6619aecf33026ec
Logistech1730 <Logistech1730@gmail.com>
2024-09-04 13:47:28 -0300

    Primeira atualização, com o banco de dados 04-09-24;

--- Banco de dados/Script BD - Tabelas .sql
+++ Banco de dados/Script BD - Tabelas .sql
@@ -3,51 +3,51 @@ USE logistech;
 
 CREATE TABLE Cadastro (
 idCadastro INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(30),
-cnpj_cpf VARCHAR(14),  -- Colocar como (essencial,  funcional) 
-data_nasc date,
+nome VARCHAR(30) NOT NULL,
+cnpj_cpf VARCHAR(14) NOT NULL, 
+data_nasc DATE,
 senha VARCHAR(20),
 email VARCHAR(40),
-genero char(1),
-constraINT chkgenero check(genero in('F','M','O'))
+genero CHAR(1),
+CONSTRAINT chkgenero CHECK(genero IN('F','M','O'))
 );
 
-insert INTo Cadastro values 
-(default, 'atheus', '54667081867','1999-01-08','matheus123','matheus@gmail.com','F'),
-(default, 'Robert', '52229087321' , '1999-02-21', 'matheus123', 'robert@gmail.com','M');
+INSERT INTO Cadastro VALUES 
+(DEFAULT, 'atheus', '54667081867','1999-01-08','matheus123','matheus@gmail.com','F'),
+(DEFAULT, 'Robert', '52229087321' , '1999-02-21', 'matheus123', 'robert@gmail.com','M');
 
-select * from Cadastro;
+SELECT * FROM Cadastro;
 DESC Cadastro;
 
-create table Arduino(
-IdSensorChecagem INT primary key auto_increment,
-distancia float,
-tempo time,
+CREATE TABLE Arduino(
+IdSensorChecagem INT PRIMARY KEY AUTO_INCREMENT,
+distancia FLOAT,
+tempo TIME,
 datachecagem DATETIME
 );
 
-alter table arduino add column datachecagem date;
+ALTER TABLE arduino ADD COLUMN datachecagem DATE;
 
-insert INTo Arduino values 
-(default, 13, '3:29:21', '2024-08-29');
+INSERT INTO Arduino VALUES 
+(DEFAULT, 13, '3:29:21', '2024-08-29');
 
-create table Estatistica(
+CREATE TABLE Estatistica(
 idEstatistica INT PRIMARY KEY AUTO_INCREMENT,
 data_horario DATETIME,
 modelo VARCHAR (30),
 quantidade INT
 );
 
-insert INTo Estatistica (data_horario, modelo, quantidade) value
+INSERT INTO Estatistica (data_horario, modelo, quantidade) VALUES
 ("2010-05-21 20:25:50", "Micro-ATX", 10),
 ("2014-06-24 17:20:15", "Starndard-ATX", 30),
 ("2022-09-05 20:05:20", "Mini-ATX", 100),
 ("2019-08-23 13:45:21", "Nano-ITX", 15),
 ("2017-10-17 14:46:20", "Pico-ITX", 23);
 
-select * from Estatistica;
+SELECT * FROM Estatistica;
 
-alter table Estatistica 
-	modify column modelo VARCHAR(20);
+ALTER TABLE Estatistica 
+	MODIFY COLUMN modelo VARCHAR(20);
     
 DESC Estatistica;
