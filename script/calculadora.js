005c064951c2a49c194d97a6f465be253d8852f4
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2024-10-28 11:17:03 -0300

    Modal de resultado do simulador financeiro

--- script/calculadora.js
+++ script/calculadora.js
@@ -4,10 +4,20 @@ function analisar(){
   var rendaMensal = Number(ipt_renda.value);
   var rendaCalculada = (unidades * valor)
   if (rendaCalculada > rendaMensal){
-      div_mensagem.innerHTML = `Seu faturamento mensal é de <br> <b style=color:red> R$${rendaMensal} </b> <br> <span><b style=color:red>R$${rendaMensal - rendaCalculada} </b>abaixo do valor esperado </span> <br>Pode indicar um possivel extravio de produtos em seu armazem <br> <br> Para averiguar o possível problema entre em contato conosco.`
+    modal_resultado_simulador.style.display = 'flex';
+    modal_resultado.style.display = 'flex';
+    mensagem_simulador.innerHTML = `Seu faturamento mensal é de <br> <b style=color:red> R$${rendaMensal} </b> <br> <span><b style=color:red>R$${rendaMensal - rendaCalculada} </b>abaixo do valor esperado </span> <br>Pode indicar um possivel extravio de produtos em seu armazem <br> <br> Para averiguar o possível problema entre em contato conosco.`
   } else if (rendaCalculada < rendaMensal){
-      div_mensagem.innerHTML = `Sua renda mensal é de <br> <b style=color:red> R$${rendaMensal - rendaCalculada} </b> <br> Está acima do esperado, <p style=color:green> O que pode ser uma boa noticia </p> <p style=color:red> ou algum erro grave no calculo da renda mensal </p> para averiguar esse possivel problema entre em contato conosco`
+    modal_resultado_simulador.style.display = 'flex';
+    modal_resultado.style.display = 'flex';
+    mensagem_simulador.innerHTML = `Sua renda mensal é de <br> <b style=color:red> R$${rendaMensal - rendaCalculada} </b> <br> Está acima do esperado, <p style=color:green> O que pode ser uma boa noticia </p> <p style=color:red> ou algum erro grave no calculo da renda mensal </p> para averiguar esse possivel problema entre em contato conosco`
   } else if (rendaCalculada == rendaMensal){
-      div_mensagem.innerHTML = `Sua renda mensal está de acordo com sua produção<br> <b style=color:green> Não há nenhum problema atualmente na detecção de entrada e saida de seus produtos </b> e caso queira saber mais sobre como melhorar sua produção entre em contato conosco`
+    modal_resultado_simulador.style.display = 'flex';
+    modal_resultado.style.display = 'flex';
+    mensagem_simulador.innerHTML = `Sua renda mensal está de acordo com sua produção<br> <b style=color:green> Não há nenhum problema atualmente na detecção de entrada e saida de seus produtos </b> e caso queira saber mais sobre como melhorar sua produção entre em contato conosco`
   }
 }
+function fecharResultado(){
+    modal_resultado_simulador.style.display = 'none';
+    modal_resultado.style.display = 'none';
+  }
