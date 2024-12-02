c749511b1bc54735e37ae5a3e922499a5fdc34a7
VitorAlmeida01 <vitor.almeida@sptech.school>
2024-12-02 13:46:05 -0300

    fix: filtro de data no historico tras informações do dia atual

--- src/models/registroModel.js
+++ src/models/registroModel.js
@@ -13,7 +13,7 @@ function listarTodosRegistros(fkEmpresa) {
 function listarRegistrosPorData(dataInicial, dataFinal, fkEmpresa) {
     var instrucaoSql = `
     SELECT 
-    esteira.departamento AS Esteira, registro.distancia AS AlturaDetectada, registro.dataRegistro AS DataRegistro FROM esteira JOIN sensor ON sensor.fkEsteira = esteira.idEsteira JOIN registro ON registro.fkSensor = sensor.idSensor JOIN empresa ON esteira.fkEmpresa = empresa.idEmpresa WHERE empresa.idEmpresa = ${fkEmpresa} AND registro.dataRegistro BETWEEN '${dataInicial}' AND '${dataFinal}' ORDER BY registro.dataRegistro;
+    esteira.departamento AS Esteira, registro.distancia AS AlturaDetectada, registro.dataRegistro AS DataRegistro FROM esteira JOIN sensor ON sensor.fkEsteira = esteira.idEsteira JOIN registro ON registro.fkSensor = sensor.idSensor JOIN empresa ON esteira.fkEmpresa = empresa.idEmpresa WHERE empresa.idEmpresa = ${fkEmpresa} AND DATE(registro.dataRegistro) BETWEEN '${dataInicial}' AND '${dataFinal}' ORDER BY registro.dataRegistro;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql); 
